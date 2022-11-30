d94fb46332d62b765c235212085d03b9bb25164f
Luke Bobrowski <lukebobrowski66@gmail.com>
2022-11-29 18:52:57 -0800

    Added characters and weapons together

--- zombie_game.py
+++ zombie_game.py
@@ -5,21 +5,24 @@ import json
 with open("Resources/characters.json", "r") as f:
     char_info = json.load(f)
 
-
+with open("Resources/weapons.json", "r") as w:
+    weap_info = json.load(w)
 
 class Character:
     def __init__(self, name, weapon=None):
 
         self.name = name
         self.health = 100
-        self.weapon: Weapon = weapon
-        self.damage = self.weapon.damage
+        self.weapon = weap_info[weapon]
+        self.weapon_name = self.weapon["name"]
+        self.damage = self.weapon["damage"]
         self.level = 1
 
     
     def dialogue(self):
         """Introduces the charater(s)"""
         print(f"My name is {self.name}")
+        print(f"My weapon is {self.weapon_name}")
 
 characters = {}
 random_number_of_charaters = random.randint(1,3)
@@ -39,16 +42,7 @@ for character in characters.values():
 # print(characters["Jeff"].health)
 
 
-class Weapon:
-    def __init__(self, name, damage, weapon_range):
-        self.name = name
-        self.damage = damage
 
 
 
 
-army_knife = Weapon("Army Knife", 10)
-brass_knuckles = Weapon("Brass Knuckles", 25)
-crossbow = Weapon("Crossbow", 25)
-slingshot = Weapon("Slingshot", 15)
-sword = Weapon("Sword", 35)
