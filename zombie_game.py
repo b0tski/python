fc41d2cfdc3c70d3326331048d59b3b1275e9ace
Luke Bobrowski <lukebobrowski66@gmail.com>
2022-12-10 21:18:45 -0800

    added a change weapon method + good vars

--- zombie_game.py
+++ zombie_game.py
@@ -1,48 +1,59 @@
-import random 
+import random
 import json
 
 
 with open("Resources/characters.json", "r") as f:
-    char_info = json.load(f)
+    characters_json = json.load(f)
 
 with open("Resources/weapons.json", "r") as w:
-    weap_info = json.load(w)
+    weapons_json = json.load(w)
+
 
 class Character:
-    def __init__(self, name, weapon=None):
+    def __init__(self, name: str, weapon_key: str = None):
 
-        self.name = name
-        self.health = 100
-        self.weapon = weap_info[weapon]
-        self.weapon_name = self.weapon["name"]
-        self.damage = self.weapon["damage"]
-        self.level = 1
+        self.name: str = name
+        self.health: int = 100
+        self.active_weapon_key: str = weapon_key
+        self.weapon_info: dict = weapons_json[self.active_weapon_key]
+        self.weapon_name: str = self.weapon_info["name"]
+        self.weapon_damage: int = self.weapon_info["damage"]
+        self.level: int = 1
 
-    
+    def change_weapon(self, new_weapon_key: str):
+
+        self.active_weapon_key: str = new_weapon_key
+        self.weapon_info: dict = weapons_json[self.active_weapon_key]
+        self.weapon_name: str = self.weapon_info["name"]
+        self.weapon_damage: int = self.weapon_info["damage"]
+
+
     def dialogue(self):
         """Introduces the charater(s)"""
         print(f"My name is {self.name}")
         print(f"My weapon is {self.weapon_name}")
+        print(f"It does {self.weapon_damage} DAMAGE!")
+
 
 characters = {}
-random_number_of_charaters = random.randint(1,3)
+random_number_of_charaters = random.randint(1, 3)
 
 for x in range(random_number_of_charaters):
-    random_character = random.choice(list(char_info.keys()))
+    random_character = random.choice(list(characters_json.keys()))
+    # While selectd character has already been chosen/ pick a new character
     while random_character in characters.keys():
-        random_character = random.choice(list(char_info.keys()))
-    characters[random_character] = Character(random_character, char_info[random_character]["default_weapons"])
+        random_character = random.choice(list(characters_json.keys()))
+    characters[random_character] = Character(
+        random_character, characters_json[random_character]["default_weapons"])
 
 
-for character in characters.values():
-    character.dialogue()
+# for character in characters.values():
+#     character.dialogue()
 
-#HOW TO TAKE DAMAGE AWAY FROM CHARACTERS
-# characters["Jeff"].health -=1
-# print(characters["Jeff"].health)
+# HOW TO TAKE DAMAGE AWAY FROM CHARACTERS
+current_character: Character = characters[random_character]
 
-
-
-
-
-
+current_character.change_weapon("GUN_vandal")
+print(current_character.name)
+print(current_character.weapon_info)
+current_character.dialogue()
