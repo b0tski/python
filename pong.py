69a5240681643ef44a5d0f582ce8d5a70ba7bbbd
b0tski <118492958+b0tski@users.noreply.github.com>
2023-01-03 14:34:45 -0500

    Update pong.py
    added players and movement to the characters

--- pong.py
+++ pong.py
@@ -1,34 +1,116 @@
-# Luke Bobrowski
-# 9 December 2022
+# LUKE BOBROWSKI
+# 2 DECEMBER 2023
+
+import pygame as py
+
+
+py.init()
+clock = py.time.Clock()
+
+WIDTH = 800
+HEIGHT = 500
+screen = py.display.set_mode((WIDTH,HEIGHT))
+
+
+# COLOR
+GRAY = (60, 64, 77)
+PLAYER_WHITE = (206, 209, 219)
+DARK_GRAY = (27, 31, 46)
+
+
+py.display.set_caption("PONG")
+
+# STARTING BUTTON
+button_width = 50
+button_height = 75
+
+
+button_boolean = True 
+
+#PLAYER 1 PHYSICS
+p1_x = 50
+p1_y = 180
+
+p1_width = 20
+p1_height = 150
+
+p1_vel = 3
+
+#PLAYER 2 PHYSICS
+
+p2_x = 750
+p2_y = 180
+
+p2_width = 20
+p2_height = 150
+
+p2_vel = 3
+
+#BALL PYISICS
+ball_x = 385
+ball_y = 250
+ball_width = 12
+ball_height = 12
+ball_speed = 3
 
 
 
-# Simple pygame program
 
-# Import and initialize the pygame library
-import pygame
-pygame.init()
+run = True
 
-# Set up the drawing window
-screen = pygame.display.set_mode([500, 500])
+while run:
 
-# Run until the user asks to quit
-running = True
-while running:
+    clock.tick(60)
 
-    # Did the user click the window close button?
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+    for event in py.event.get():
 
-    # Fill the background with white
-    screen.fill((255, 255, 255))
+        if event.type == py.QUIT:
+            run = False
 
-    # Draw a solid blue circle in the center
-    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+    keys = py.key.get_pressed()
 
-    # Flip the display
-    pygame.display.flip()
 
-# Done! Time to quit.
-pygame.quit()
+
+    # STARTING SCREEN
+    # while button_boolean == True:
+    #     starting_button = py.draw.rect(screen, PLAYER_WHITE, (400, 250, button_width, button_height))
+    #     if keys[py.CONTROLLER_BUTTON_LEFTSTICK] :
+    #         pass
+    
+    #PLAYER 1
+    if keys[py.K_w] and p1_y > 0:
+
+        p1_y -= p1_vel
+
+    if keys[py.K_s] and p1_y < 500 - p1_height:
+
+        p1_y += p1_vel
+
+
+    #PLAYER 2 
+    if keys[py.K_i] and p2_y > 0:
+
+        p2_y -= p2_vel
+
+
+    if keys[py.K_k] and p2_y < 500 - p2_height:
+
+        p2_y += p2_vel
+
+
+    screen.fill(GRAY)
+
+    
+
+    player_1 = py.draw.rect(screen, PLAYER_WHITE, (p1_x, p1_y, p1_width, p1_height))
+    player_2 = py.draw.rect(screen, PLAYER_WHITE, (p2_x, p2_y, p2_width, p2_height))
+
+    ball = py.draw.rect(screen, PLAYER_WHITE, (ball_x, ball_y, 20, 20))
+
+
+
+
+    py.display.update()
+
+
+py.quit()
