003c1750f7fee29d0690e42e4e2016aa85d0924b
b0tski <118492958+b0tski@users.noreply.github.com>
2023-02-03 13:46:58 -0500

    Update pong.py
    new code

--- pong.py
+++ pong.py
@@ -1,6 +1,7 @@
 
 
 import pygame as py
+import random
 
 
 py.init()
@@ -29,7 +30,7 @@ button_height = 75
 button_boolean = True 
 
 #PLAYER 1 PHYSICS
-p1_x = 50
+p1_x = 49
 p1_y = 180
 
 p1_width = 20
@@ -49,32 +50,38 @@ player_2 = py.Rect(p2_x, p2_y, p2_width, p2_height)
 p2_vel = 3
 p2_score = 0
 
+# MID LINE
+midline = py.Rect(399,0,10,500)
+
 #BALL PYISICS
 
+first_bounce = 1
 ball_x = 385
 ball_y = 250
 ball_width = 20
 ball_height = 20
 ball = py.Rect(ball_x, ball_y, ball_width, ball_height)
-ball_speed = [5,1]
+ball_speed = [7,0]
 
 # Functions 
 
 def score_p1():
 
-    text_score1 = font.render(str(p1_score), True, (0,0,0))
-    screen.blit(text_score1, (435, 450))
+    text_score1 = font.render(str(p1_score), True, PLAYER_WHITE)
+    screen.blit(text_score1, (325, 450))
 
 
 def score_p2():
-    text_score2 = font.render(str(p2_score), True, (0, 0, 0))
-    screen.blit(text_score2, (400, 450))
+    text_score2 = font.render(str(p2_score), True, PLAYER_WHITE)
+    screen.blit(text_score2, (450, 450))
+
+FPS = 100
 
 run = True
 
 while run:
 
-    clock.tick(60)
+    clock.tick(FPS)
 
     for event in py.event.get():
 
@@ -122,110 +129,55 @@ while run:
     # RIGHT SIDE
     if ball[0] > WIDTH - ball[2]:
         ball_speed[0] *= -1
-        score_p1()
+        p1_score +=1
+
 
     #LEFT SIDE
     if ball[0] < 0:
         ball_speed[0] *= -1
-        score_p2()
+        p2_score += 1
+
+
+    rand_direction = random.randint(0,1)
 
 
     # Ball collisioin 
     if player_1.colliderect(ball):
-        ball_speed[0] *= -1
+        if first_bounce == 1:
+            if rand_direction == 1:
+                ball_speed[1] = -2            
+            if rand_direction == 0:
+                ball_speed[1] = 2
+        first_bounce -= 1
+        if rand_direction == 1:
+            ball_speed[0] *= -1  
+            ball_speed[1] *= -1
+        if rand_direction == 0:
+            ball_speed[0] *= -1
     
     if player_2.colliderect(ball):
-        ball_speed[0] *= -1
+        if first_bounce == 1:
+            if rand_direction == 1:
+                ball_speed[1] = -2            
+            if rand_direction == 0:
+                ball_speed[1] = 2
+        first_bounce -= 1               
+        if rand_direction == 1:
+            ball_speed[0] *= -1  
+            ball_speed[1] *= -1
+        if rand_direction == 0:
+            ball_speed[0] *= -1
 
-
+    score_p1()
+    score_p2()
 
     
     # DRAWING
     py.draw.rect(screen, PLAYER_WHITE, player_1)
     py.draw.rect(screen, PLAYER_WHITE, player_2)
+    py.draw.rect(screen,PLAYER_WHITE, midline)
 
     py.draw.rect(screen, PLAYER_WHITE, ball)
-
-
-
-
-    py.display.update()
-
-
-py.quit()
-
-p1_vel = 3
-
-#PLAYER 2 PHYSICS
-
-p2_x = 750
-p2_y = 180
-
-p2_width = 20
-p2_height = 150
-
-p2_vel = 3
-
-#BALL PYISICS
-ball_x = 385
-ball_y = 250
-ball_width = 12
-ball_height = 12
-ball_speed = 3
-
-
-
-
-run = True
-
-while run:
-
-    clock.tick(60)
-
-    for event in py.event.get():
-
-        if event.type == py.QUIT:
-            run = False
-
-    keys = py.key.get_pressed()
-
-
-
-    # STARTING SCREEN
-    # while button_boolean == True:
-    #     starting_button = py.draw.rect(screen, PLAYER_WHITE, (400, 250, button_width, button_height))
-    #     if keys[py.CONTROLLER_BUTTON_LEFTSTICK] :
-    #         pass
-    
-    #PLAYER 1
-    if keys[py.K_w] and p1_y > 0:
-
-        p1_y -= p1_vel
-
-    if keys[py.K_s] and p1_y < 500 - p1_height:
-
-        p1_y += p1_vel
-
-
-    #PLAYER 2 
-    if keys[py.K_i] and p2_y > 0:
-
-        p2_y -= p2_vel
-
-
-    if keys[py.K_k] and p2_y < 500 - p2_height:
-
-        p2_y += p2_vel
-
-
-    screen.fill(GRAY)
-
-    
-
-    player_1 = py.draw.rect(screen, PLAYER_WHITE, (p1_x, p1_y, p1_width, p1_height))
-    player_2 = py.draw.rect(screen, PLAYER_WHITE, (p2_x, p2_y, p2_width, p2_height))
-
-    ball = py.draw.rect(screen, PLAYER_WHITE, (ball_x, ball_y, 20, 20))
 
 
 
